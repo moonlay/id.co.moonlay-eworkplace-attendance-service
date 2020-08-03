bf902f3ca211cc3db0db9af4a0ee1bf9ada07927
leslieaula <cristiano.ulo@gmail.com>
2020-08-03 17:48:02 +0700

    Fix Migrations Initialize

--- EWorkplaceAbsensiService.WebApi/Migrations/20200803104318_Initial.cs
+++ EWorkplaceAbsensiService.WebApi/Migrations/20200803104318_Initial.cs
@@ -5,10 +5,47 @@ using System.Collections.Generic;
 
 namespace EWorkplaceAbsensiService.WebApi.Migrations
 {
-    public partial class TimeSheetModuleApi : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Absensis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    Approval = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApprovalByAdmin = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CheckIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckOut = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAgent = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedAgent = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DeletedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DeletedUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HeadDivision = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModifiedAgent = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    LastModifiedUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Absensis", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Activity",
                 columns: table => new
@@ -177,6 +214,9 @@ namespace EWorkplaceAbsensiService.WebApi.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Absensis");
+
             migrationBuilder.DropTable(
                 name: "Activity");
 
